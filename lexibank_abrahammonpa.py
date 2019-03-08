c10a1f761d74238f58accb572c0f0d0429beca09
MacyL <wumeishin@gmail.com>
2019-03-08 16:56:40 +0100

    still lots of error, but I will fix the python script later

--- lexibank_abrahammonpa.py
+++ lexibank_abrahammonpa.py
@@ -43,7 +43,7 @@ class Dataset(NonSplittingDataset):
             data.extend(temp)
 
         # build cldf
-        languages, concepts = [], {}
+        check_languages, concepts = [], {}
         with self.cldf as ds:
             for concept in self.concepts:
                 ds.add_concept(
@@ -52,35 +52,35 @@ class Dataset(NonSplittingDataset):
                         Concepticon_ID=concept['CONCEPTICON_ID'],
                         Concepticon_Gloss=concept['CONCEPTICON_GLOSS']
                         )
-                concepts[concept['ENGLISH']] = concept['ID']
+                concepts[concept['ENGLISH']] = concept['NUMBER']
             for language in self.languages:
-                if language['Language_in_Wiktionary']:
+                if language['Language_in_Wiktionary'] !='':
                     ds.add_language(
                             ID=slug(language['Language']),
                             Glottocode=language['Glottolog'],
                             Name=language['Language']
                             )
-                languages.append((language['Language_in_Wiktionary'])
+                    check_languages.append(language['Language_in_Wiktionary'])
             
             ds.add_sources(*self.raw.read_bib())
             missing = defaultdict(int)
-            for entry in tqdm(data, desc='cldfify the data'):
-                if entry[''] not in concepts:
-                    missing[entry['']] +=1
-                else:
-                    for langauge in languages:
-                        value = self.lexemes.get(entry[language],
+            for c, entry in tqdm(enumerate(data), desc='cldfify the data'):
+                if entry[''] in concepts.keys():
+                    for language in check_languages:
+                        if language in entry.keys():
+                            value = self.lexemes.get(entry[language],
                             entry[language]
                             )
+                        else:
+                            value=''
                         if value.strip():
                             ds.add_lexemes(
-                                Language_ID=languages[language],
-                                Parameter_ID=concepts[concept],
-                                Value=self.lexemes.get(value, value),
+                                Language_ID=slug(language),
+                                Parameter_ID=concepts[entry['']],
+                                Value=value,
                                 Source=['Abraham2018']
                                 )
-            for i, m in enumerate(missing):
-                print(str(i+1)+'\t'+m)
-
+                else:
+                    missing[entry['']] +=1
 
 
