8ac9ab623b6f3df38fa287cb76b476f5449425fe
lingulist <mattis.list@lingpy.org>
2019-08-01 15:29:28 +0200

    adding test

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # coding: utf-8
 from __future__ import unicode_literals
 from lingpy import *
@@ -6,7 +5,5 @@ from lexibank_abrahammonpa import Dataset
 
 wl = Wordlist.from_cldf(Dataset().cldf_dir.joinpath('cldf-metadata.json'))
 
-=======
->>>>>>> 755919caf231a387d70930ba16ac21f1c56f2096
 def test_valid(cldf_dataset, cldf_logger):
     assert cldf_dataset.validate(log=cldf_logger)
