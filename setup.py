d93d8c67a070df5e846e8ff172f9425e99bad0ed
chrzyki <christoph@functor-argument.net>
2019-11-11 12:44:33 +0100

    Release prep

--- setup.py
+++ setup.py
@@ -2,31 +2,20 @@ from setuptools import setup
 import json
 
 
-with open('metadata.json') as fp:
+with open("metadata.json") as fp:
     metadata = json.load(fp)
 
 
 setup(
-    name='lexibank_abrahammonpa',
+    name="lexibank_abrahammonpa",
     version="1.0",
-    description=metadata['title'],
-    license=metadata.get('license', ''),
-    url=metadata.get('url', ''),
-    py_modules=['lexibank_abrahammonpa'],
+    description=metadata["title"],
+    license=metadata.get("license", ""),
+    url=metadata.get("url", ""),
+    py_modules=["lexibank_abrahammonpa"],
     include_package_data=True,
     zip_safe=False,
-    entry_points={
-        'lexibank.dataset': [
-            'abrahammonpa=lexibank_abrahammonpa:Dataset',
-        ]
-    },
-    install_requires=[
-        'beautifulsoup4>=4.7.1',
-        'pylexibank>=2.0',
-    ],
-    extras_require={
-        'test': [
-            'pytest-cldf',
-        ],
-    },
+    entry_points={"lexibank.dataset": ["abrahammonpa=lexibank_abrahammonpa:Dataset"]},
+    install_requires=["beautifulsoup4>=4.7.1", "pylexibank>=2.1"],
+    extras_require={"test": ["pytest-cldf"]},
 )
